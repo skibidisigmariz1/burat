216101a4f2d2b1fd017652012d9a1fcfc6642d80
sigma <miruakasha5@gmail.com>
2024-12-20 16:40:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     'random2.mp4',
     'random3.mp4',
     'random4.mp4',
-    'random5.mp4'// Add more video files here
+    'random5.mp4' // Add more video files here
   ];
   let currentVideoIndex = 0;
 
@@ -13,6 +13,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   const prevButton = document.getElementById('prevButton');
   const nextButton = document.getElementById('nextButton');
+  const playButton = document.getElementById('playButton');
+  const pauseButton = document.getElementById('pauseButton');
 
   prevButton.addEventListener('click', () => {
     currentVideoIndex = (currentVideoIndex - 1 + videoSources.length) % videoSources.length;
@@ -24,6 +26,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
     currentVideoIndex = (currentVideoIndex + 1) % videoSources.length;
     videoPlayer.src = videoSources[currentVideoIndex];
     videoPlayer.play();
+  });
+
+  playButton.addEventListener('click', () => {
+    videoPlayer.play();
+  });
+
+  pauseButton.addEventListener('click', () => {
+    videoPlayer.pause();
   });
 
   videoPlayer.addEventListener('ended', () => {
